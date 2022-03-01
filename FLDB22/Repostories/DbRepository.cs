4604eae174c89f52845750b0f9ccdf196d148d7d
Erik Öberg <erik.oberg@miun.se>
2022-03-01 14:47:08 +0100

    Add project files.

---
+++ FLDB22/Repostories/DbRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace FLDB22.Repostories
+{
+    internal class DbRepository
+    {
+        private string _connectionString;
+
+        public DbRepository()
+        {
+            var config = new ConfigurationBuilder().AddUserSecrets<DbRepository>()
+                        .Build();
+
+            _connectionString = config.GetConnectionString("dbConn");
+        }
+    }
+}
